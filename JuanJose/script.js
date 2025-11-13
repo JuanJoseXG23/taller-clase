7fc9354ff5ab46a76390bcd0d657e3377584b82e
Juan Jose Gomez <juanjosexg23@gmail.com>
2025-11-13 12:01:00 -0500

    Ejercio 2 y se añaden alertas antes de cada ejercicio

--- JuanJose/script.js
+++ JuanJose/script.js
@@ -1,5 +1,5 @@
 //Ejercicio 1//
-
+alert("PROGRAMA QUE CALCULA TOTAL DE VENTAS")
 let precio1 = parseFloat(prompt("Ingresa el precio del primer producto:"));
 let precio2 = parseFloat(prompt("Ingresa el precio del segundo producto:"));
 let precio3 = parseFloat(prompt("Ingresa el precio del tercer producto:"));
@@ -7,3 +7,12 @@ let precio3 = parseFloat(prompt("Ingresa el precio del tercer producto:"));
 let total = precio1 + precio2 + precio3;
 
 alert("El total a pagar es: $" + total);
+
+//ejercicio 2
+alert("PROGRAMA QUE CALCULA AREA ")
+let ancho = parseFloat(prompt("Ingresa el ancho de la pared en metros:"));
+let alto = parseFloat(prompt("Ingresa el alto de la pared en metros:"));
+
+let area = ancho * alto;
+
+alert("El área de la pared es: " + area + " metros cuadrados.");
