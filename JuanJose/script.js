2932dd686c898fc47171980204cd791824df0b2a
Juan Jose Gomez <juanjosexg23@gmail.com>
2025-11-13 11:51:42 -0500

    Ejercicio 1

---
+++ JuanJose/script.js
@@ -0,0 +1,9 @@
+//Ejercicio 1//
+
+let precio1 = parseFloat(prompt("Ingresa el precio del primer producto:"));
+let precio2 = parseFloat(prompt("Ingresa el precio del segundo producto:"));
+let precio3 = parseFloat(prompt("Ingresa el precio del tercer producto:"));
+
+let total = precio1 + precio2 + precio3;
+
+alert("El total a pagar es: $" + total);
